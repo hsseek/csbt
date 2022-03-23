13013b340c349d311495ceb6f5d4825e207dc17d
Soldier87 <sunskham@gmail.com>
2022-03-23 15:23:26 +0900

    Change string formatting
    - Add a class to hold constants.
    - Remove weights on picking a confirmation message for the help command.
    - Change string formatting grammars.
    - Add a method to append 'ㅋ'.
    - Add nudity directions.

--- common.py
+++ common.py
@@ -1,30 +1,10 @@
 import random
 import time
-from datetime import datetime
 import os
-
-
-def log(message: str, path: str, has_tst: bool = True):
-    dir_path = split_on_last_pattern(path, '/')[0]
-    check_dir_exists(dir_path)
-
-    with open(path, 'a') as f:
-        if has_tst:
-            message += '\t(%s)' % get_str_time()
-        f.write(message + '\n')
-    print(message)
-
-
-def get_str_time() -> str:
-    return str(datetime.now()).split('.')[0]
 
 
 def pause_briefly(min_pause: float = 0.4, max_pause: float = 2.4):
     time.sleep(random.uniform(min_pause, max_pause))
-
-
-def format_quiet_chat(content: str) -> str:
-    return '(%s)' % content
 
 
 def check_dir_exists(dir_path: str):
@@ -40,15 +20,6 @@ def read_from_file(path: str):
         return f.read().strip('\n')
 
 
-def get_date_difference(tst_str: str) -> int:
-    try:
-        date = datetime.strptime(tst_str, '%Y.%m.%d')  # 2021.11.07
-        now = datetime.now()
-        return (now - date).days
-    except Exception as e:
-        print('(%s) The timestamp did not match the format: %s.' % (e, tst_str))
-
-
 def build_tuple(path: str):
     content = read_from_file(path)
     return tuple(content.split('\n'))
@@ -60,16 +31,3 @@ def build_tuple_of_tuples(path: str):
     for line in lines:
         info.append(tuple(line.split(',')))
     return tuple(info)
-
-
-def get_elapsed_sec(start_time) -> float:
-    end_time = datetime.now()
-    return (end_time - start_time).total_seconds()
-
-
-# Split on the pattern, but always returning a list with length of 2.
-def split_on_last_pattern(string: str, pattern: str) -> ():
-    last_piece = string.split(pattern)[-1]  # domain.com/image.jpg -> jpg
-    leading_chunks = string.split(pattern)[:-1]  # [domain, com/image]
-    leading_piece = pattern.join(leading_chunks)  # domain.com/image
-    return leading_piece, last_piece  # (domain.com/image, jpg)
--- csbt.py
+++ csbt.py
@@ -1,15 +1,16 @@
 # https://core.telegram.org/bots/api
 import random
 import re
-
+import telegram
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 import common
 import logging
 
-global bot
 
-bot_token, bot_username = common.build_tuple('token.pv')
+class Constants:
+    BOT_TOKEN, BOT_USERNAME = common.build_tuple('token.pv')
+
 
 # Enable logging.
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -18,39 +19,33 @@ commands = common.build_tuple_of_tuples('help.pv')
 
 
 def command_help(update: Update, context: CallbackContext):
-    weight_large = 3
-    weight_small = 1
     command_help_str = ''
     for help_item in commands:
         command, desc = help_item
-        command_help_str += '/%s\t%s\n' % (command, desc)
+        command_help_str += '/{}\t{}\n'.format(command, desc)
 
     # Reply to the command.
     update.message.reply_text(str.strip(command_help_str))
 
     # Send a confirmation message occasionally.
-    confirmation = common.build_tuple('confirmation.pv')
-    # Weights of confirmation messages
-    weights = []
-    for i in range(len(confirmation) - 1):
-        # Weights of 3 for normal messages
-        weights.append(weight_large)
-    # Weights 1 for the special message
-    weights.append(weight_small)
+    confirmation = random.choice(common.build_tuple('confirmation.pv'))
 
     # Append the confirmation message.
-    if random.randint(1, 10) <= 1:
-        confirmation_str = random.choices(population=confirmation, weights=weights)[0]
-        # Add a couple of ㅎ or not.
-        for i in range(random.randint(0, 2)):
-            confirmation_str += 'ㅎ'
+    if random.uniform(0, 1) < 0.1:
         common.pause_briefly(2, 4)
-        context.bot.send_message(chat_id=update.message.chat_id, text=confirmation_str)
+        context.bot.send_message(chat_id=update.message.chat_id, text=add_k(confirmation))
 
 
-def alarm(context: CallbackContext) -> None:
+def add_k(msg: str) -> str:
+    for i in range(random.randint(3, 5)):
+        msg += 'ㅋ'
+    return msg
+
+
+def halt(context: CallbackContext) -> None:
     job = context.job
-    context.bot.send_message(job.context, text=common.format_quiet_chat('설정된 타이머 시간에 도달했습니다.'))
+    # TODO: Combine with send_bot_message()
+    context.bot.send_message(job.context, text='`설정된 타이머 시간에 도달했습니다.`', parse_mode=telegram.ParseMode.MARKDOWN)
 
 
 def remove_job_if_exists(chat_id: str, context: CallbackContext) -> bool:
@@ -73,17 +68,38 @@ def order(update: Update, context: CallbackContext) -> None:
             give_rubbing_duration(update, context)
     except (IndexError, ValueError):
         name = common.read_from_file('name.pv')
-        update.message.reply_text('메시지를 처리할 수 없습니다. %s에게 보고하고 문제가 해결될 때까지 기다리세요.' % name)
+        update.message.reply_text('메시지를 처리할 수 없습니다. {}에게 보고하고 문제가 해결될 때까지 기다리세요.'.format(name))
 
 
 def give_rubbing_posture(update: Update, context: CallbackContext):
-    random_direction = random.choice(common.build_tuple_of_tuples('01-0.pv'))
-    for line in random_direction:
+    is_naked = False
+    if random.uniform(0, 1) < 0.3:
+        common.pause_briefly(max_pause=0.8)
+        nudity_direction = random.choice(common.build_tuple('01-0.pv'))
+        context.bot.send_message(chat_id=update.message.chat_id, text=nudity_direction)
+        is_naked = True
+
+    random_direction = random.choice(common.build_tuple_of_tuples('01-1.pv'))
+
+    # Add a pause before picking.
+    common.pause_briefly(2.4, 3.6)
+    for i, line in enumerate(random_direction):
+        if is_naked and i == 0:
+            line = '그리고 다 벗었으면 ' + line
         common.pause_briefly(max_pause=1.2)
         context.bot.send_message(chat_id=update.message.chat_id, text=line)
     common.pause_briefly(1.6, 2.4)
-    common_directions = common.build_tuple('01-1.pv')
-    for line in common_directions:
+
+    # Give common directions.
+    common_direction_optional = random.choice(common.build_tuple('01-2.pv'))
+    # Give an order sometimes.
+    if random.uniform(0, 1) < 0.3:
+        common.pause_briefly(max_pause=1.2)
+        context.bot.send_message(chat_id=update.message.chat_id, text=common_direction_optional)
+    # Always give the orders.
+    common_directions_required = common.build_tuple('01-3.pv')
+    for line in common_directions_required:
+        common.pause_briefly(max_pause=1.2)
         context.bot.send_message(chat_id=update.message.chat_id, text=line)
 
 
@@ -97,33 +113,43 @@ def give_rubbing_duration(update: Update, context: CallbackContext):
     if direction_str[0].isdigit():
         minutes = int(re.search(r'\d+', direction_str).group())
         set_timer(update, context, minutes)
-        print('Timer set for %d' % minutes)
+        print('Timer set for {}'.format(minutes))
 
 
 def set_timer(update: Update, context: CallbackContext, minutes: int) -> None:
     # Add a job to the queue.
     chat_id = update.message.chat_id
     job_removed = remove_job_if_exists(str(chat_id), context)
-    context.job_queue.run_once(alarm, minutes * 60, context=chat_id, name=str(chat_id))
+
+    seconds = minutes * 60
+    context.job_queue.run_once(halt, seconds, context=chat_id, name=str(chat_id))
     # TODO: '~분 남았습니다.' 알림
     # https://stackoverflow.com/questions/52556939, https://stackoverflow.com/questions/47167193
 
-    update.message.reply_text(common.format_quiet_chat('%d분 타이머가 설정되었습니다.') % minutes)
+    common.pause_briefly(0.8, 1.2)
+    send_bot_message(update, context, '{:d}분 타이머가 설정되었습니다.'.format(minutes), True)
     if job_removed:
-        update.message.reply_text(common.format_quiet_chat('기존에 설정된 타이머는 취소합니다.'))
+        send_bot_message(update, context, '기존 설정 타이머는 취소합니다.', True)
+
+
+def send_bot_message(update: Update, context: CallbackContext, msg: str, is_reply: bool = False):
+    if is_reply:
+        update.message.reply_text(text='`{}`'.format(msg), parse_mode=telegram.ParseMode.MARKDOWN)
+    else:
+        context.bot.send_message(chat_id=update.message.chat_id, text=msg, parse_mode=telegram.ParseMode.MARKDOWN)
 
 
 def cancel_timer(update: Update, context: CallbackContext) -> None:
     # Remove the scheduled job if the user changed their mind.
     chat_id = update.message.chat_id
     job_removed = remove_job_if_exists(str(chat_id), context)
-    text = '타이머가 취소되었습니다.' if job_removed else '활성화된 타이머가 없습니다.'
-    update.message.reply_text(common.format_quiet_chat(text))
+    msg = '타이머가 취소되었습니다.' if job_removed else '활성화된 타이머가 없습니다.'
+    send_bot_message(update, context, msg, True)
 
 
 def error(update: Updater, context: CallbackContext):
     # Log Errors caused by Updates
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
+    logger.warning('Update {} caused error {}'.format(update, context.error))
 
 
 def order_1(update: Update, context: CallbackContext):
@@ -140,7 +166,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater(bot_token, use_context=True)
+    updater = Updater(Constants.BOT_TOKEN, use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
